9555c24ba4bad6a89166867dcf7a3eee48c88b41
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-11-24 16:07:57 +0400

    v.1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,53 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Profile profile = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))){
+
+            String line;
+
+            String name = "";
+            Integer age = null;
+            String email = "";
+            Long phone = null;
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                String[] words = line.split(":");
+
+                if (words.length != 2){
+                    continue;
+                }
+
+                if (words[0].equals("Name")){
+                    name = words[1].trim();
+                } else if (words[0].equals("Age")) {
+                    age = Integer.parseInt(words[1].trim());
+                } else if (words[0].equals("Email")) {
+                    email = words[1].trim();
+                } else if (words[0].equals("Phone")) {
+                    phone = Long.parseLong(words[1].trim());
+                }
+            }
+
+            profile = new Profile(name,age,email,phone);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return profile;
     }
 }
