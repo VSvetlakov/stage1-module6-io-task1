748addf8ed2fe4b2a22c6257e36b225848bebae8
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-11-24 16:20:21 +0400

    v.2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -42,10 +42,8 @@ public class FileReader {
 
             profile = new Profile(name,age,email,phone);
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error while reading file",e.getCause());
         }
 
         return profile;
